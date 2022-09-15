4febe73d743b1ea2489a46add40e02e5306fa0ca
Scott Lin <scott.lin@tw.synaptics.com>
2022-09-15 10:52:26 +0800

    Remove pinormos-streamer-router/streamer-router-deb/DEBIAN/control

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ set -e
 rm -fr *.deb
 cp control pinormos-streamer-router/streamer-router-deb/DEBIAN/.
 pinormos-streamer-router/gen-deb.sh
-rm -fr streamer_router_deb.tar.gz
-tar -zcf streamer_router_deb.tar.gz *
+rm -fr pinormos-streamer-router/streamer-router-deb/DEBIAN/control
